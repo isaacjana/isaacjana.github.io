54cc53c7edeebed68f3e7215193253eea0118d6e
isaacjana <72616440+isaacjana@users.noreply.github.com>
2026-01-06 07:49:50 +0800

    Create sw.js

---
+++ flowstate/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'liferpg-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://code.jquery.com/jquery-3.6.0.min.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
