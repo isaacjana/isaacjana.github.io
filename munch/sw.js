14c5a42090b2d6548911d4a7f1c24fe20a2bf878
isaacjana <72616440+isaacjana@users.noreply.github.com>
2026-01-08 11:58:46 +0800

    Update sw.js

--- munch/sw.js
+++ munch/sw.js
@@ -1,15 +1,13 @@
-const CACHE_NAME = 'munch-v2';
+const CACHE_NAME = 'munch-v3';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
     self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request).catch(() => caches.match('./index.html'));
-        })
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
     );
 });
