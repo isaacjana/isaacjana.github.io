6d00c94f1b8091fdec98fad385210831198d7000
isaacjah <isaacjah@sains.com.my>
2026-01-15 12:18:57 +0800

    test

--- qNext/firebase-config.js
+++ qNext/firebase-config.js
@@ -11,7 +11,6 @@ import { getDatabase, ref, set, update, onValue, runTransaction, push, child, ge
 const firebaseConfig = {
     apiKey: "AIzaSyDsGbfRlXxqUwLHXbGcwRYOvuygTPgTeMA",
     authDomain: "penny-wise-e482e.firebaseapp.com",
-    databaseURL: "https://penny-wise-e482e-default-rtdb.firebaseio.com",
     projectId: "penny-wise-e482e",
     storageBucket: "penny-wise-e482e.firebasestorage.app",
     messagingSenderId: "504425521894",
